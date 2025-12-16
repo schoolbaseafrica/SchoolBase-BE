41a5e5d6b27b2886eaa2269d00b0ef3e8c558c99
Elijah Arhinful <32518272+elijaharhinful@users.noreply.github.com>
2025-12-16 21:59:05 +0000

    refactor: use minio for image upload service (#12)
    * wip: commit before dev pull
    
    * chore: replace cloudinary instances throughout the codebase
    
    * fix: fix term tests failures

--- src/modules/upload/upload.module.ts
+++ src/modules/upload/upload.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { CloudinaryService } from './services/cloudinary.service';
+import { MinioService } from './services/minio.service';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 
 @Module({
   imports: [ConfigModule],
   controllers: [UploadController],
-  providers: [UploadService, CloudinaryService],
-  exports: [UploadService, CloudinaryService],
+  providers: [UploadService, MinioService],
+  exports: [UploadService, MinioService],
 })
 export class UploadModule {}
