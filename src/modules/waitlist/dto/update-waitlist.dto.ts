9ed200bb60ac2141a59c9979bddac194bb1d5f35
Praiz001 <oshilimpraisek@gmail.com>
2025-12-18 20:06:28 +0100

    refactor(waitlist): remove waitlist module, controller, service, and DTOs from the project

--- src/modules/waitlist/dto/update-waitlist.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-
-import { CreateWaitlistDto } from './create-waitlist.dto';
-
-export class UpdateWaitlistDto extends PartialType(CreateWaitlistDto) {}
